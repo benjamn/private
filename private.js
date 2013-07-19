0e8443ee3cf09497442b3e6429d064a66ce69acb
Ben Newman <bn@cs.stanford.edu>
2013-07-19 11:28:39 -0400

    Make private.js safe to use in non-ES5 environments.
    This requires polyfilling Object.defineProperty and Object.create when
    those methods are not built in.

--- private.js
+++ private.js
@@ -1,10 +1,16 @@
 "use strict";
 
-var defProp = Object.defineProperty;
-var create = Object.create;
-var rand = Math.random;
-var uniqueKeys = create(null);
+var defProp = Object.defineProperty || function(obj, name, desc) {
+    // Normal property assignment is the best we can do if
+    // Object.defineProperty is not available.
+    obj[name] = desc.value;
+};
 
+// For functions that will be invoked using .call or .apply, we need to
+// define those methods on the function objects themselves, rather than
+// inheriting them from Function.prototype, so that a malicious or clumsy
+// third party cannot interfere with the functionality of this module by
+// redefining Function.prototype.call or .apply.
 function makeSafeToCall(fun) {
     defProp(fun, "call", { value: fun.call });
     defProp(fun, "apply", { value: fun.apply });
@@ -15,12 +21,34 @@ var hasOwn = makeSafeToCall(Object.prototype.hasOwnProperty);
 var numToStr = makeSafeToCall(Number.prototype.toString);
 var strSlice = makeSafeToCall(String.prototype.slice);
 
+var cloner = function(){};
+var create = Object.create || function(prototype, properties) {
+    cloner.prototype = prototype || null;
+    var obj = new cloner;
+
+    // The properties parameter is unused by this module, but I want this
+    // shim to be as complete as possible.
+    if (properties)
+        for (var name in properties)
+            if (hasOwn.call(properties, name))
+                defProp(obj, name, properties[name]);
+
+    return obj;
+};
+
+var rand = Math.random;
+var uniqueKeys = create(null);
+
 function makeUniqueKey() {
+    // Collisions are highly unlikely, but this module is in the business
+    // of making guarantees rather than safe bets.
     do var uniqueKey = strSlice.call(numToStr.call(rand(), 36), 2);
     while (hasOwn.call(uniqueKeys, uniqueKey));
     return uniqueKeys[uniqueKey] = uniqueKey;
 }
 
+// External users might find this function useful, but it is not necessary
+// for the typical use of this module.
 defProp(exports, "makeUniqueKey", {
     value: makeUniqueKey
 });
