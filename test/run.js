5bef8c1065324cce23e53c9c995f04da66a53ff8
Ben Newman <bn@cs.stanford.edu>
2014-02-19 13:42:32 -0500

    Basic tests of autoForget interface.

--- test/run.js
+++ test/run.js
@@ -19,7 +19,7 @@ assert.deepEqual(acc1(obj), { bar: "baz" });
 
 try {
     acc1(42);
-    assert.ok(false, "should have thrown");
+    throw new Error("threw wrong error");
 } catch (err) {
     assert.ok(err);
 }
@@ -45,6 +45,73 @@ assert.ok(!hasOwn.call(newSecret, "bar"));
 newSecret.bar = "zxcv";
 assert.strictEqual(oldSecret.bar, "asdf");
 assert.strictEqual(acc2(obj).bar, "zxcv");
+
+var context = {};
+acc2.autoForget(function() {
+    assert.strictEqual(this, context);
+    assert.strictEqual(acc2(obj).bar, "zxcv");
+
+    try {
+        acc2.forget(obj);
+        throw new Error("threw wrong error");
+    } catch (err) {
+        assert.strictEqual(
+            err.message,
+            "attempted to forget object owned by another autoForget context"
+        );
+    }
+
+    acc2(obj).bar = "uiop";
+
+    var s1 = acc2(context);
+    s1.name = "context";
+    assert.deepEqual(s1, { name: "context" });
+
+    acc2.forget(context);
+    var s2 = acc2(context);
+    assert.notStrictEqual(s1, s2);
+    assert.deepEqual(s2, {});
+    acc2(context).xxx = "yyy";
+    assert.strictEqual(s2.xxx, "yyy");
+
+    var obj2 = {};
+    acc2.forget(obj2); // No-op.
+
+    acc2.autoForget(function() {
+        assert.strictEqual(s2, acc2(context));
+        assert.strictEqual(acc2(context).xxx, "yyy");
+
+        acc2.forget(obj2); // No-op.
+        acc2(obj2).name = "obj2";
+    });
+
+    acc2(obj2).xxx = "zzz";
+    assert.strictEqual(acc2(obj2).name, void 0);
+    assert.strictEqual(acc2(obj2).xxx, "zzz");
+    acc2.forget(obj2);
+    assert.strictEqual(acc2(obj2).xxx, void 0);
+
+    assert.strictEqual(acc2(context).xxx, "yyy");
+}, context);
+
+assert.deepEqual(acc2(context), {});
+assert.strictEqual(acc2(obj).bar, "uiop");
+
+var strictAcc = makeAccessor(true);
+
+try {
+    strictAcc(context);
+    throw new Error("threw wrong error");
+} catch (err) {
+    assert.strictEqual(
+        err.message,
+        "attempted to use accessor outside autoForget context"
+    );
+}
+
+assert.deepEqual(strictAcc.autoForget(function() {
+    return strictAcc(context);
+}), {});
 
 var green = "\033[32m";
 var reset = "\033[0m";
